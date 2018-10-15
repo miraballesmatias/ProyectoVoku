dfedd98ffbc80eac6088ca290f8a5eef31d7167e
Gaston bugnar <gastonbugnar@gmail.com>
2018-10-15 15:06:15 -0300

    vokuId global para la aplicación

--- public/firestore-init.js
+++ public/firestore-init.js
@@ -1,16 +1,16 @@
 var db = firebase.firestore();
 db.settings({ timestampsInSnapshots: true });
 
-// var vokuId = null;
-// if (localStorage.getItem("voku_id")) { vokuId = localStorage.getItem("voku_id") }
-// else if (document.cookie && document.cookie.split("voku_id=")[0]) { vokuId = document.cookie.split("voku_id=")[0] }
-// else {
-//     db.collection("users").add({
-//         timestamp: firebase.firestore.FieldValue.serverTimestamp(),
-//         visits: []
-//     }).then((docRef) => {
-//         vokuId = docRef.id;
-//         localStorage.setItem("voku_id", vokuId);
-//         document.cookie = `voku_id=${vokuId}; expires=Tue, 19 Jan 2038 03:14:07 UTC`
-//     })
-// }
+var vokuId = null;
+if (localStorage.getItem("voku_id")) { vokuId = localStorage.getItem("voku_id") }
+else if (document.cookie && document.cookie.split("voku_id=")[0]) { vokuId = document.cookie.split("voku_id=")[0] }
+else {
+    db.collection("users").add({
+        timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+        visits: []
+    }).then((docRef) => {
+        vokuId = docRef.id;
+        localStorage.setItem("voku_id", vokuId);
+        document.cookie = `voku_id=${vokuId}; expires=Tue, 19 Jan 2038 03:14:07 UTC`
+    })
+}
